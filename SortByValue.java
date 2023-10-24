bdaf015d1a40a29be24112a289463cc6204d7b79
Aparna <aparnageorge2401@gmail.com>
2023-10-24 15:20:55 +0100

    Comments added

--- SortByValue.java
+++ SortByValue.java
@@ -8,6 +8,13 @@ public class SortByValue implements Comparator<String> {
         this.map = map;
     }
 
+    /**
+     *
+     * @param o1 the key of the first pair to be compared.
+     * @param o2 the key of the second pair to be compared.
+     * uses the default compareTo method to compare between the values associated with each key
+     * @return
+     */
     @Override
     public int compare(String o1, String o2) {
         return map.get(o1).compareTo(map.get(o2));
--- SortUKTownNames.java
+++ SortUKTownNames.java
@@ -2,6 +2,9 @@ import java.util.*;
 
 public class SortUKTownNames {
     public static void main(String[] args) {
+        /**
+         * Adding towns to the map
+         */
         Map<String,Integer> townMap = new HashMap<>();
         townMap.put("London",10);
         townMap.put("Manchester",7);
@@ -11,8 +14,15 @@ public class SortUKTownNames {
         /*List<Map.Entry<String,Integer>> townMapArray = new ArrayList<Map.Entry<String,Integer>>();
         townMapArray.addAll(townMap.entrySet());
         townMapArray.sort(Map.Entry.comparingByKey());*/
+        /**
+         * Creating a treemap to hold the sorted map. A custom comparator was created to sort by the value
+         * fields instead of the keys
+         */
         TreeMap<String, Integer> sortedTownMap = new TreeMap<String, Integer>(new SortByValue(townMap));
         sortedTownMap.putAll(townMap);
+        /**
+         * An iterator to traverse the sorted map containing the town details
+         */
         Iterator<Map.Entry<String,Integer>>townMapIterator = sortedTownMap.entrySet().iterator();
         while(townMapIterator.hasNext()){
             System.out.println(townMapIterator.next());
